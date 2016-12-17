ab36b98194a34c080d442051d61c0f4f55dbb647
Innocent <25cent9@gmail.com>
2016-12-17 01:17:40 -0600

    I added some elements and tested them out

--- mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
@@ -1,20 +1,18 @@
 package com.tripidevs.swoly;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -22,6 +20,7 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import static android.widget.Toast.LENGTH_SHORT;
 import static com.tripidevs.swoly.R.id;
 
 
@@ -38,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditText eachSide = (EditText) findViewById(id.txtBoxEachSide);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -59,35 +69,20 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            //Creating a new intent for the settings activity
-            Intent i = new Intent(this, Settings.class);
-            startActivity(i);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void buttonClick(View view) {
+    public void tisClick(View view) {
         Button button = (Button) view;
         EditText sides = (EditText) findViewById(id.txtBoxEachSide);
-//        EditText MaxWeight = (EditText) findViewById(id.editTxtMaxWeight);
-        float eachSideWeight;
-        int maxWeight;
-        float percentage = Float.parseFloat(button.getText().toString())/100;
-        Context context = view.getContext();
-        try
-        {
-//            maxWeight = Integer.parseInt(MaxWeight.getText().toString());
-//            eachSideWeight = ((maxWeight*percentage)-45)/2;
-//            sides.setText(String.valueOf(eachSideWeight));
-        }
-        catch(Exception e)
-        {
-            CharSequence msg = "Please enter a number";
-            Toast toast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
-            toast.show();
-        }
+        CharSequence text = sides.getText();
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+        toast.show();
+        sides.setText(button.getText().toString());
     }
 
     /**
