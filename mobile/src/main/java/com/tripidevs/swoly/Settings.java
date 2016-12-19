44d6c2b78d4846c137918c4ad9d77924d63deedd
Innocent <25cent9@gmail.com>
2016-12-18 22:06:50 -0600

    Added the settings
    Settings xml file is in there and we can change between the two

--- mobile/src/main/java/com/tripidevs/swoly/Settings.java
+++ mobile/src/main/java/com/tripidevs/swoly/Settings.java
@@ -2,6 +2,14 @@ package com.tripidevs.swoly;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by Innocent Niyibizi on 12/18/2016.
