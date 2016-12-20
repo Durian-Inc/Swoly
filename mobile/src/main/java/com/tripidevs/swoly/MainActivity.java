5cc8cf0a41edca32b7ea9268cd3b0ea2fee93d55
Clay McGinnis <claymav@yahoo.com>
2016-12-20 00:29:45 -0600

    added new layout foundation

--- mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -37,17 +39,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditText eachSide = (EditText) findViewById(id.txtBoxEachSide);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -80,16 +71,16 @@ public class MainActivity extends AppCompatActivity {
     public void buttonClick(View view) {
         Button button = (Button) view;
         EditText sides = (EditText) findViewById(id.txtBoxEachSide);
-        EditText MaxWeight = (EditText) findViewById(id.editTxtMaxWeight);
+//        EditText MaxWeight = (EditText) findViewById(id.editTxtMaxWeight);
         float eachSideWeight;
         int maxWeight;
         float percentage = Float.parseFloat(button.getText().toString())/100;
         Context context = view.getContext();
         try
         {
-            maxWeight = Integer.parseInt(MaxWeight.getText().toString());
-            eachSideWeight = ((maxWeight*percentage)-45)/2;
-            sides.setText(String.valueOf(eachSideWeight));
+//            maxWeight = Integer.parseInt(MaxWeight.getText().toString());
+//            eachSideWeight = ((maxWeight*percentage)-45)/2;
+//            sides.setText(String.valueOf(eachSideWeight));
         }
         catch(Exception e)
         {
