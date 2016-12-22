3848c498d44e99aa766a8649f13be1bd0ef58730
Clay McGinnis <claymav@yahoo.com>
2016-12-21 23:00:49 -0600

    Created wear application, functions just like mobile, not pretty right now but totally functional.

---
+++ wear/src/main/java/com/tripidevs/swoly/Constant.java
@@ -0,0 +1,11 @@
+package com.tripidevs.swoly;
+
+/*
+ * Created by maver on 12/20/2016.
+ */
+
+public class Constant {
+    public static int max = 135;
+    public static float percent = .55f;
+
+}
--- wear/src/main/java/com/tripidevs/swoly/MainActivity.java
+++ wear/src/main/java/com/tripidevs/swoly/MainActivity.java
@@ -3,22 +3,58 @@ package com.tripidevs.swoly;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.SpinnerAdapter;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+    }
+
+    public void changeWeight(View v)
+    {
+        boolean plus = v.equals(findViewById(R.id.maxplus));
+        TextView maxWeight = (TextView) findViewById(R.id.max);
+        if(plus)
+            Constant.max+=5;
+        if (!plus&&(((((Constant.max)-5) * Constant.percent) - 45) / 2) > 0)
+            Constant.max-=5;
+        maxWeight.setText(String.valueOf(Constant.max));
+        refreshEachSide();
+    }
+
+    public void changePercent(View v)
+    {
+        boolean plus = v.equals(findViewById(R.id.percentplus));
+        TextView percent = (TextView) findViewById(R.id.percent);
+        if(plus)
+            Constant.percent+=.05f;
+        else if ((((((Constant.max)-5) * Constant.percent) - 45) / 2) > 0)
+            Constant.percent-=.05f;
+        String percentText = String.valueOf(Math.round(Constant.percent*100))+"%";
+        percent.setText(percentText);
+        refreshEachSide();
+    }
+
+    public void refreshEachSide() {
+        TextView sides = (TextView) findViewById(R.id.eachside);
+        double eachSideWeight;
+        eachSideWeight = ((Constant.max * Constant.percent) - 45) / 2;
+        if (eachSideWeight > 0)
+            sides.setText(String.valueOf(eachSideWeight));
     }
 }
